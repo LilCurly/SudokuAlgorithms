edf6063651db76e69b37c37287f2ce351d93edf7
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-06 16:59:08 +0100

    (en cours) Initiation d'une grille de sudoku 4x4 avec des valeurs vides

--- src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4.java
+++ src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4.java
@@ -1,7 +1,8 @@
 package be.technifutur.devmob.sudoku.sudoku4x4;
 
 public class Sudoku4x4 {
-    private char[][] values = new char[4][4];
+    public static final char EMPTY = 0;
+    private Position4x4[][] values = new Position4x4[4][4];
 
     public Sudoku4x4() {
         initiate();
@@ -10,12 +11,13 @@ public class Sudoku4x4 {
     public void initiate() {
         for(int i = 0; i < values.length; i++) {
             for(int j = 0; j < values.length; j++) {
-                values[i][j] = '0';
+                values[i][j] = add(new Position4x4(i, j), EMPTY);
             }
         }
     }
 
-    public void add(Position4x4 p, char val) {
+    public Position4x4 add(Position4x4 p, char val) {
 
+        return p;
     }
 }
