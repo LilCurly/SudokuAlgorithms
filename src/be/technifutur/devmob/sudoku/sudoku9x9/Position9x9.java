c49564218ef772fd0e794d2052487f9b46d335ee
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-08 08:56:39 +0100

    Adding initial Position9x9 class and the test class that goes with it in order to go for TDD

---
+++ src/be/technifutur/devmob/sudoku/sudoku9x9/Position9x9.java
@@ -0,0 +1,5 @@
+package be.technifutur.devmob.sudoku.sudoku9x9;
+
+public class Position9x9 {
+
+}
---
+++ tests/be/technifutur/devmob/sudoku/sudoku9x9/Position9x9Test.java
@@ -0,0 +1,7 @@
+package be.technifutur.devmob.sudoku.sudoku9x9;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Position9x9Test {
+
+}
