d22a395949d8e23e2ed201525f25101822c56c85
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-08 10:51:33 +0100

    Finalisation des tests de Position9x9 et implémentation pour leurs réussites

--- src/be/technifutur/devmob/sudoku/sudoku9x9/Position9x9.java
+++ src/be/technifutur/devmob/sudoku/sudoku9x9/Position9x9.java
@@ -1,5 +1,41 @@
 package be.technifutur.devmob.sudoku.sudoku9x9;
 
 public class Position9x9 {
+    private int pos;
 
+    public Position9x9(int pos) {
+        this.pos = pos;
+    }
+
+    public Position9x9(int col, int row) {
+        setPos(col, row);
+    }
+
+    private void setPos(int col, int row) {
+        this.pos = col + (row * 9);
+    }
+
+    public int getPos() {
+        return this.pos;
+    }
+
+    public int getRow() {
+        return (int) Math.floor(this.pos / 9);
+    }
+
+    public int getCol() {
+        return this.pos % 9;
+    }
+
+    public int getRowSector() {
+        return (int) Math.floor(this.pos / 27);
+    }
+
+    public int getColSector() {
+        return (int) Math.floor(getCol() / 3);
+    }
+
+    public int getSector() {
+        return getColSector() + (getRowSector() * 3);
+    }
 }
--- tests/be/technifutur/devmob/sudoku/sudoku9x9/Position9x9Test.java
+++ tests/be/technifutur/devmob/sudoku/sudoku9x9/Position9x9Test.java
@@ -1,7 +1,109 @@
 package be.technifutur.devmob.sudoku.sudoku9x9;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Position9x9Test {
 
+    /*
+        Testing if getPos() returns the correct position that was given to the constructor
+     */
+    @Test
+    void testGetPosReturnsCorrectValueFromPosGivenToConstructor() {
+        Position9x9 p = new Position9x9(35);
+        assertEquals(35, p.getPos(), String.format("Pos 35 should give Pos 35 but got %d", p.getPos()));
+    }
+
+    /*
+        Testing if getPos() returns the correct position when Position9x9 is instantiated with a row and a column
+     */
+    @Test
+    void testGetPosReturnsCorrectValueFromRowColGivenToConstructor() {
+        Position9x9 p = new Position9x9(4, 5);
+        assertEquals(49, p.getPos(), String.format("Col 4 Row 5 should give pos 49 but go %d", p.getPos()));
+    }
+
+    /*
+        Testing if getRow() returns the correct row when Position9x9 is instantiated with a position
+     */
+    @Test
+    void testGetRowReturnsCorrectRowFromPosGivenToConstructor() {
+        Position9x9 p1 = new Position9x9(56);
+        Position9x9 p2 = new Position9x9(19);
+        Position9x9 p3 = new Position9x9(74);
+        assertEquals(6, p1.getRow(), String.format("Pos 56 should give Row 6 but got %d", p1.getRow()));
+        assertEquals(2, p2.getRow(), String.format("Pos 19 should give Row 2 but got %d", p2.getRow()));
+        assertEquals(8, p3.getRow(), String.format("Pos 74 should give Row 8 but got %d", p3.getRow()));
+    }
+
+    /*
+        Testing if getCol() returns the correct col when Position9x9 is instantiated with a position
+     */
+    @Test
+    void testGetColReturnsCorrectColFromPosGivenToConstructor() {
+        Position9x9 p1 = new Position9x9(43);
+        Position9x9 p2 = new Position9x9(0);
+        Position9x9 p3 = new Position9x9(80);
+        assertEquals(7, p1.getCol(), String.format("Pos 43 should give Col 7 but got %d", p1.getCol()));
+        assertEquals(0, p2.getCol(), String.format("Pos 0 should give Col 0 but got %d", p2.getCol()));
+        assertEquals(8, p3.getCol(), String.format("Pos 80 should give Col 8 but got %d", p3.getCol()));
+    }
+
+    /*
+        Testing if getRow() returns the correct row when Position9x9 is instantiated with a row and a column
+     */
+    @Test
+    void testGetRowReturnsCorrectRowFromRowColGivenToConstructor() {
+        Position9x9 p = new Position9x9(4, 5);
+        assertEquals(5, p.getRow(), String.format("Col 4 Row 5 should give Row 5 but got %d", p.getRow()));
+    }
+
+    /*
+        Testing if getCol() returns the correct col when Position9x9 is instantiated with a row and a column
+     */
+    @Test
+    void testGetColReturnsCorrectColFromRowColGivenToConstructor() {
+        Position9x9 p = new Position9x9(7, 4);
+        assertEquals(7, p.getCol(), String.format("Col 7 Row 4 should give Col 7 but got %d", p.getCol()));
+    }
+
+    /*
+        Testing if getRowSector() returns the correct Row in the form of 3x3 sectors
+     */
+    @Test
+    void testGetRowSectorReturnsCorrectRowSector() {
+        Position9x9 p1 = new Position9x9(25);
+        Position9x9 p2 = new Position9x9(37);
+        Position9x9 p3 = new Position9x9(59);
+        assertEquals(0, p1.getRowSector(), String.format("Pos 25 should give Row Sector 0 but got %d", p1.getRowSector()));
+        assertEquals(1, p2.getRowSector(), String.format("Pos 37 should give Row Sector 1 but got %d", p2.getRowSector()));
+        assertEquals(2, p3.getRowSector(), String.format("Pos 59 should give Row Sector 2 but got %d", p3.getRowSector()));
+    }
+
+    /*
+        Testing if getColSector() returns the correct Col in the form of 3x3 sectors
+     */
+    @Test
+    void testGetColSectorReturnsCorrectColSector() {
+        Position9x9 p1 = new Position9x9(10);
+        Position9x9 p2 = new Position9x9(49);
+        Position9x9 p3 = new Position9x9(70);
+        assertEquals(0, p1.getColSector(), String.format("Pos 10 should give Col Sector 0 but got %d", p1.getColSector()));
+        assertEquals(1, p2.getColSector(), String.format("Pos 49 should give Col Sector 1 but got %d", p2.getColSector()));
+        assertEquals(2, p3.getColSector(), String.format("Pos 70 should give Col Sector 2 but got %d", p3.getColSector()));
+    }
+
+    /*
+        Testing if getSector() returns the correct Sector in the form of 3x3 sectors
+     */
+    @Test
+    void testGetSectorReturnsCorrectSector() {
+        Position9x9 p1 = new Position9x9(19);
+        Position9x9 p2 = new Position9x9(40);
+        Position9x9 p3 = new Position9x9(80);
+        assertEquals(0, p1.getSector(), String.format("Pos 19 should give Sector 0 but got %d", p1.getSector()));
+        assertEquals(4, p2.getSector(), String.format("Pos 40 should give Sector 4 but got %d", p2.getSector()));
+        assertEquals(8, p3.getSector(), String.format("Pos 80 should give Sector 8 but got %d", p3.getSector()));
+    }
 }
