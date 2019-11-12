dd3f6daf9f7d0be6585a449141533815af4ab8c5
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-12 08:36:16 +0100

    Adding Sudoku 9x9 model and the test class

--- src/be/technifutur/devmob/sudoku/sudoku9x9/Sudoku9x9.java
+++ src/be/technifutur/devmob/sudoku/sudoku9x9/Sudoku9x9.java
@@ -1,4 +1,16 @@
 package be.technifutur.devmob.sudoku.sudoku9x9;
 
 public class Sudoku9x9 {
+    public static final char EMPTY = '.';
+    private char[] values = new char[9*9];
+
+    public Sudoku9x9() {
+
+    }
+
+    public void initiate() {
+        for(int i = 0; i < values.length; i++) {
+            values[i] = EMPTY;
+        }
+    }
 }
