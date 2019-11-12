44aa8ad1f2d633773fb615046b33cf7a9deb92f4
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-12 14:32:23 +0100

    Created GridGenerator util class to create fake inputs for the UserFake class and created the tests for the generate() function

---
+++ src/be/technifutur/devmob/sudoku/utils/GridGenerator.java
@@ -0,0 +1,11 @@
+package be.technifutur.devmob.sudoku.utils;
+
+public class GridGenerator {
+    public static String[] generate(int limit) {
+        String[] result = new String[limit * limit];
+        for(int i = 0; i < limit * limit; i++) {
+            result[i] = String.format("%d.%d.%d", (int) Math.ceil((double) (i+1)/limit), (i%limit)+1, (i%limit)+1);
+        }
+        return result;
+    }
+}
---
+++ tests/be/technifutur/devmob/sudoku/utils/GridGeneratorTest.java
@@ -0,0 +1,30 @@
+package be.technifutur.devmob.sudoku.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GridGeneratorTest {
+
+    /*
+        Testing if the generate(9) function returns a valid 81 of length String array
+     */
+    @Test
+    void testGenerate9ReturnsValidStringArray() {
+        String[] values = GridGenerator.generate(9);
+        assertEquals(81, values.length);
+        assertEquals("1.1.1", values[0]);
+        assertEquals("9.9.9", values[80]);
+    }
+
+    /*
+        Testing if the generate(4) function returns a valid 16 of length String array
+     */
+    @Test
+    void testGenerate4ReturnsValidStringArray() {
+        String[] values = GridGenerator.generate(4);
+        assertEquals(16, values.length);
+        assertEquals("1.1.1", values[0]);
+        assertEquals("4.4.4", values[15]);
+    }
+}
