b3ac80e0ef3192bb1d455e5bbc724fbe16e05c23
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-18 13:48:41 +0100

    complete() method to complete a sudoku 4x4 is done, i have to make it generic later

--- src/be/technifutur/devmob/sudoku/utils/AutoCompletor.java
+++ src/be/technifutur/devmob/sudoku/utils/AutoCompletor.java
@@ -1,10 +1,18 @@
 package be.technifutur.devmob.sudoku.utils;
 
+import be.technifutur.devmob.sudoku.Cellule;
 import be.technifutur.devmob.sudoku.sudoku4x4.Sudoku4x4;
 
 public class AutoCompletor {
     public static void complete(Sudoku4x4 model) {
-
+        Cellule[] cells = model.getValues();
+        char[] vals = generatePossibleValues((int) Math.sqrt(cells.length));
+        for(int i = 0; i < cells.length; i++) {
+            int index = 0;
+            while(!cells[i].setValue(vals[index])) {
+                index++;
+            }
+        }
     }
 
     public static char[] generatePossibleValues(int length) {
