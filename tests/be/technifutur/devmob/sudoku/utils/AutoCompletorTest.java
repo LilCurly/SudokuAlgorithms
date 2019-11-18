cf687f980a4896747cc04cc601fcb60018c2ec65
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-18 12:34:07 +0100

    Added test n°1 : testing if generatePossibleValues() generates an array of 1 char that is '1'

--- tests/be/technifutur/devmob/sudoku/utils/AutoCompletorTest.java
+++ tests/be/technifutur/devmob/sudoku/utils/AutoCompletorTest.java
@@ -1,7 +1,18 @@
 package be.technifutur.devmob.sudoku.utils;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AutoCompletorTest {
 
+    /*
+        Test N°1 : Testing if generatePossibleValues() generates an array of 1 char that is '1'
+     */
+    @Test
+    void testGeneratePossibleValuesReturnsArrayOfOneCharThatIs1() {
+        char[] vals = AutoCompletor.generatePossibleValues(1);
+        assertEquals(1, vals.length);
+        assertEquals('1', vals[0]);
+    }
 }
