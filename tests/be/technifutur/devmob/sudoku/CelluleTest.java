db2116c5cc6022a1900eace3962d77488b55fe24
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-18 09:27:58 +0100

    Test n°1 added : testing if adding a new ValueSet adds it to the ArrayList of the cell

--- tests/be/technifutur/devmob/sudoku/CelluleTest.java
+++ tests/be/technifutur/devmob/sudoku/CelluleTest.java
@@ -1,7 +1,20 @@
 package be.technifutur.devmob.sudoku;
 
+import be.technifutur.devmob.sudoku.utils.ValueSet;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CelluleTest {
 
+    /*
+        Test N°1 : Testing if adding a new ValueSet to the Cell correctly adds it to the ArrayList
+     */
+    @Test
+    void testAddNewValueSetAddsItToArrayList() {
+        Cellule cell = new Cellule();
+        assertEquals(0, cell.getValueSet().size());
+        cell.addValueSet(new ValueSet(4));
+        assertEquals(1, cell.getValueSet().size());
+    }
 }
