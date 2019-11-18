0e460e0b9f8e7adbd36221628ebfa23b04d721a7
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-18 16:42:58 +0100

    Added the possibility to update a position that is already set

--- src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4Controller.java
+++ src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4Controller.java
@@ -18,24 +18,37 @@ public class Sudoku4x4Controller {
         view.showSudoku();
         while(!isOver) {
             String entry;
-            Pattern p = Pattern.compile("^\\d\\.\\d\\.\\d");
-            Matcher m;
+            Pattern add = Pattern.compile("(\\d\\.\\d\\.\\d)");
+            Pattern delete = Pattern.compile("d(\\d\\.\\d)");
+            Pattern update = Pattern.compile("u(\\d\\.\\d\\.\\d)");
+            Matcher addMatcher;
+            Matcher deleteMatcher;
+            Matcher updateMatcher;
             do {
                 entry = view.prompt("Entrez une valeur dans le format : {ligne}.{colonne}.{valeur} pour entrer une nouvelle valeur ou 0 pour quitter : ");
-                m = p.matcher(entry);
-            } while(!entry.equals("0") && !m.matches());
+                addMatcher = add.matcher(entry);
+                deleteMatcher = delete.matcher(entry);
+                updateMatcher = update.matcher(entry);
+            } while(!entry.equals("0") && !addMatcher.matches() && !deleteMatcher.matches() && !updateMatcher.matches());
             if(entry.equals("0")) {
                 isOver = true;
             }
-            else {
-                Pattern p2 = Pattern.compile("\\.");
-                String[] regexValues = p2.split(entry);
+            else if (addMatcher.matches() || updateMatcher.matches()) {
+                Matcher match = Pattern.compile("u?(?<val>\\d\\.\\d\\.\\d)").matcher(entry);
+                if(match.lookingAt()) {
+                    entry = match.group("val");
+                }
+                Pattern splitter = Pattern.compile("\\.");
+                String[] regexValues = splitter.split(entry);
                 int row = Integer.parseInt(regexValues[0]);
                 int col = Integer.parseInt(regexValues[1]);
                 char value = regexValues[2].charAt(0);
                 boolean result = false;
-                if(Position4x4.isValid(col - 1, row - 1)) {
+                if(Position4x4.isValid(col - 1, row - 1) && addMatcher.matches()) {
                     result = model.add(new Position4x4(col - 1, row - 1), value);
+                }
+                else if(Position4x4.isValid(col - 1, row - 1) && updateMatcher.matches()) {
+                    result = model.update(new Position4x4(col - 1, row - 1), value);
                 }
                 if(result) {
                     view.update();
