c9dcb21ddbb4870e241f8ae967dbe4ab5a8b6132
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-18 08:55:32 +0100

    Modified the sudoku 4x4 model to implement the cells to hold the values instead of simple chars

--- src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4.java
+++ src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4.java
@@ -1,45 +1,59 @@
 package be.technifutur.devmob.sudoku.sudoku4x4;
 
+import be.technifutur.devmob.sudoku.Cellule;
+
 public class Sudoku4x4 {
     public static final char EMPTY = '.';
-    private char[] values = new char[16];
+    private Cellule[] values = new Cellule[16];
+    private boolean locked;
 
     public Sudoku4x4() {
-        initiate();
+        locked = false;
     }
 
-    public void initiate() {
-        for(int i = 0; i < values.length; i++) {
-            values[i] = EMPTY;
-        }
+    public void setCellule(Position4x4 p, Cellule c) {
+        values[p.getPos()] = c;
+    }
+
+    public Cellule getCellule(Position4x4 p) {
+        return values[p.getPos()];
     }
 
     public boolean add(Position4x4 p, char val) {
         boolean result = false;
         if(val >= '1' && val <= '4') {
-            values[p.getPos()] = val;
-            result = true;
+            result = values[p.getPos()].setValue(val);
         }
         return result;
     }
 
     public char get(Position4x4 p) {
-        return values[p.getPos()];
+        return values[p.getPos()].getValue();
     }
 
     public boolean isComplete() {
         boolean result = true;
-        for(char c : values) {
-            if(c == EMPTY) result = false;
+        for(Cellule c : values) {
+            if(c.getValue() == EMPTY) result = false;
         }
         return result;
     }
 
-    public char[] getValues() {
+    public Cellule[] getValues() {
         return this.values;
     }
 
-    public void delete(Position4x4 p) {
-        values[p.getPos()] = EMPTY;
+//    public void delete(Position4x4 p) {
+//        values[p.getPos()] = EMPTY;
+//    }
+    public void lock() {
+        locked = true;
+        for(Cellule c : values) {
+            if(c.getValue() != EMPTY) c.lock();
+        }
+    }
+
+    public boolean isLocked() {
+        return locked;
     }
 }
