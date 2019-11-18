52d15b6a8e2ec631aeab721726ec0f492a4ffd84
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-18 12:12:29 +0100

    Creating AutoCompletor class for completing automatically the sudoku with all valid values

---
+++ src/be/technifutur/devmob/sudoku/utils/AutoCompletor.java
@@ -0,0 +1,13 @@
+package be.technifutur.devmob.sudoku.utils;
+
+import be.technifutur.devmob.sudoku.sudoku4x4.Sudoku4x4;
+
+public class AutoCompletor {
+    public static void complete(Sudoku4x4 model) {
+
+    }
+
+    private static char[] generatePossibleValues(int length) {
+
+    }
+}
