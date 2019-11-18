724d7e37882801c079ac728cb990159db8c00275
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-18 12:37:23 +0100

    Added test n°2 : Testing if generatePossibleValues() generates an array of char values that goes from '1' to the given length

--- tests/be/technifutur/devmob/sudoku/utils/AutoCompletorTest.java
+++ tests/be/technifutur/devmob/sudoku/utils/AutoCompletorTest.java
@@ -15,4 +15,17 @@ class AutoCompletorTest {
         assertEquals(1, vals.length);
         assertEquals('1', vals[0]);
     }
+
+    /*
+        Test N°2 : Testing if generatePossibleValues() generates an array of char values that goes from '1' to the given length
+     */
+    @Test
+    void testGeneratePossibleValuesReturnsArrayOfCharFrom1ToLength() {
+        char[] vals = AutoCompletor.generatePossibleValues(4);
+        assertEquals(4, vals.length);
+        assertEquals('1', vals[0]);
+        assertEquals('2', vals[1]);
+        assertEquals('3', vals[2]);
+        assertEquals('4', vals[3]);
+    }
 }
