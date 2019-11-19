12a8bfca7e7a0d4b633ca9c03e2f783ebc6f0f44
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 15:05:30 +0100

    Modified constructors to accept a max argument which specify the maximum value that it may take

--- src/be/technifutur/devmob/sudoku/PositionInvalidException.java
+++ src/be/technifutur/devmob/sudoku/PositionInvalidException.java
@@ -1,11 +1,11 @@
 package be.technifutur.devmob.sudoku;
 
 public class PositionInvalidException extends SudokuException {
-    public PositionInvalidException(int row, int col) {
-        super(String.format("Position is invalid : row should be between 1 and 4 (but was %d), col should be between 1 and 4 (but was %d)", row+1, col+1));
+    public PositionInvalidException(int row, int col, int max) {
+        super(String.format("Position is invalid : row should be between 1 and %d (but was %d), col should be between 1 and 4 (but was %d)", max, row+1, col+1));
     }
 
-    public PositionInvalidException(int pos) {
-        super(String.format("Position is invalid : pos should be between 1 and 16 (but was %d)", pos+1));
+    public PositionInvalidException(int pos, int max) {
+        super(String.format("Position is invalid : pos should be between 1 and %d (but was %d)", max, pos+1));
     }
 }
