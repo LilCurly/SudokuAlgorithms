00beeb9141b6002e059ec0e8e2ad4dca5ffefeee
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 10:58:12 +0100

    Overloaded constructor to accept only one position as argument

--- src/be/technifutur/devmob/sudoku/PositionInvalidException.java
+++ src/be/technifutur/devmob/sudoku/PositionInvalidException.java
@@ -4,4 +4,8 @@ public class PositionInvalidException extends SudokuException {
     public PositionInvalidException(int row, int col) {
         super(String.format("Position is invalid : row should be between 1 and 4 (but was %d), col should be between 1 and 4 (but was %d)", row, col));
     }
+
+    public PositionInvalidException(int pos) {
+        super(String.format("Position is invalid : pos should be between 1 and 16 (but was %d)", pos));
+    }
 }
