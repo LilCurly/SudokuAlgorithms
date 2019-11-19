5af7aa5cd6cba2d1dc897bc3108c655ed76b0c81
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 11:58:17 +0100

    Modified throws statements to throw SudokuException

--- tests/be/technifutur/devmob/sudoku/CelluleTest.java
+++ tests/be/technifutur/devmob/sudoku/CelluleTest.java
@@ -22,7 +22,7 @@ class CelluleTest {
         Test N°2 : Testing if a value can be retrieved after being added
      */
     @Test
-    void testValueCanBeRetrievedAfterBeingAdded() throws ValueAlreadyDefinedException, CellLockedException {
+    void testValueCanBeRetrievedAfterBeingAdded() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.setValue('1');
@@ -33,7 +33,7 @@ class CelluleTest {
         Test n°3 : Testing if a value can be added once with one ValueSet
      */
     @Test
-    void testValueCanBeAddedOnceWithOneValueSet() throws ValueAlreadyDefinedException, CellLockedException {
+    void testValueCanBeAddedOnceWithOneValueSet() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         assertTrue(cell.setValue('1'));
@@ -43,7 +43,7 @@ class CelluleTest {
         Test N°4 : Testing if a value cannot be added more than once with one ValueSet
      */
     @Test
-    void testValueCannotBeAddedMoreThanOnceWithOneValueSet() throws ValueAlreadyDefinedException, CellLockedException {
+    void testValueCannotBeAddedMoreThanOnceWithOneValueSet() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.setValue('2');
@@ -54,7 +54,7 @@ class CelluleTest {
         Test N°5 : Testing if a value can be added once with more than one ValueSet
      */
     @Test
-    void testValueCanBeAddedOnceWithMoreThanOneValueSet() throws ValueAlreadyDefinedException, CellLockedException {
+    void testValueCanBeAddedOnceWithMoreThanOneValueSet() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.addValueSet(new ValueSet(4));
@@ -65,7 +65,7 @@ class CelluleTest {
         Test N°6 : Testing if a value cannot be added more than once with more than one ValueSet
      */
     @Test
-    void testValueCannotBeSetMoreThanOnceWithMoreThanOneValueSet() throws ValueAlreadyDefinedException, CellLockedException {
+    void testValueCannotBeSetMoreThanOnceWithMoreThanOneValueSet() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.addValueSet(new ValueSet(4));
@@ -77,7 +77,7 @@ class CelluleTest {
         Test N°7 : Testing if two different values can be add
      */
     @Test
-    void testTwoDifferentValuesCanBeAdded() throws ValueAlreadyDefinedException, CellLockedException {
+    void testTwoDifferentValuesCanBeAdded() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.addValueSet(new ValueSet(4));
@@ -89,7 +89,7 @@ class CelluleTest {
         Test N°8 : Testing if two distinct Cells with one similar ValueSet cannot add more than once the same value
      */
     @Test
-    void testTwoCellsWithSameValueSetCannotAddSameValue() throws ValueAlreadyDefinedException, CellLockedException {
+    void testTwoCellsWithSameValueSetCannotAddSameValue() throws SudokuException {
         Cellule cell1 = new Cellule();
         Cellule cell2 = new Cellule();
         ValueSet vs = new ValueSet(4);
@@ -103,7 +103,7 @@ class CelluleTest {
         Test N°9 : Testing if two distinct Cells with one similar ValueSet can add two distinct values
      */
     @Test
-    void testTwoCellsWithSameValueSetCanAddTwoDifferentValues() throws ValueAlreadyDefinedException, CellLockedException {
+    void testTwoCellsWithSameValueSetCanAddTwoDifferentValues() throws SudokuException {
         Cellule cell1 = new Cellule();
         Cellule cell2 = new Cellule();
         ValueSet vs = new ValueSet(4);
@@ -126,7 +126,7 @@ class CelluleTest {
         Test N°11 : Testing if a Cell value cannot be set after being locked
      */
     @Test
-    void testCellValueCannotBeSetAfterBeingLocked() throws ValueAlreadyDefinedException, CellLockedException {
+    void testCellValueCannotBeSetAfterBeingLocked() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.lock();
@@ -138,7 +138,7 @@ class CelluleTest {
         Test N°12 : Testing if a Cell value cannot be update after being locked
      */
     @Test
-    void testCellValueCannotBeUpdatedAfterBeingLocked() throws ValueAlreadyDefinedException, CellLockedException {
+    void testCellValueCannotBeUpdatedAfterBeingLocked() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.lock();
@@ -150,7 +150,7 @@ class CelluleTest {
         Test N°13 : Testing if a Cell value can be modified
      */
     @Test
-    void testCellValueCanBeModifed() throws CellLockedException, ValueAlreadyDefinedException {
+    void testCellValueCanBeModifed() throws SudokuException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         assertTrue(cell.setValue('1'));
