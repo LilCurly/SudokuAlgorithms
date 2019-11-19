88364c145b0d012f05adda9f2129c64a6d2019cb
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 15:54:31 +0100

    Modified implementation to use Cellule model

--- src/be/technifutur/devmob/sudoku/sudoku9x9/Sudoku9x9.java
+++ src/be/technifutur/devmob/sudoku/sudoku9x9/Sudoku9x9.java
@@ -1,49 +1,80 @@
 package be.technifutur.devmob.sudoku.sudoku9x9;
 
+import be.technifutur.devmob.sudoku.*;
+
 public class Sudoku9x9 {
     public static final char EMPTY = '.';
-    private char[] values = new char[9*9];
+    private Cellule[] values = new Cellule[9*9];
+    private boolean locked;
 
     public Sudoku9x9() {
-        initiate();
+        locked = false;
     }
 
-    public void initiate() {
-        for(int i = 0; i < values.length; i++) {
-            values[i] = EMPTY;
-        }
-    }
-
-    public char[] getValues() {
+    public Cellule[] getValues() {
         return values;
     }
 
-    public boolean add(Position9x9 p, char value) {
-        boolean result = true;
-        if(value > '0' && value <= '9') {
-            values[p.getPos()] = value;
+    public Cellule getCellule(Position9x9 p) {
+        return this.values[p.getPos()];
+    }
+
+    public boolean add(Position9x9 p, char value) throws SudokuException {
+        boolean result = false;
+        if(this.values[p.getPos()].getValue() != EMPTY) {
+            throw new CellAlreadySetException(p.getRow(), p.getCol());
         }
-        else result = false;
+        if(value > '0' && value <= '9') {
+            result = values[p.getPos()].setValue(value);
+        }
+        else {
+            throw new ValueOutOfBoundException(value, 9);
+        }
         return result;
     }
 
     public boolean isComplete() {
         boolean result = true;
-        int index = 0;
-        while(result && index <= 80) {
-            if(values[index] == EMPTY) {
-                result = false;
-            }
-            index++;
+        for(Cellule c : this.values) {
+            if(c.getValue() == EMPTY) result = false;
         }
         return result;
     }
 
     public char get(Position9x9 p) {
-        return values[p.getPos()];
+        return values[p.getPos()].getValue();
     }
 
-    public void delete(Position9x9 p) {
-        values[p.getPos()] = EMPTY;
+    public boolean delete(Position9x9 p) throws SudokuException {
+        return values[p.getPos()].deleteValue();
+    }
+
+    public void setCellule(Position9x9 p, Cellule cell) {
+        this.values[p.getPos()] = cell;
+    }
+
+    public boolean update(Position9x9 p, char val) throws SudokuException {
+        boolean result = false;
+        if(this.values[p.getPos()].getValue() == EMPTY) {
+            throw new CellNotSetException(p.getRow(), p.getCol());
+        }
+        if(val >= '1' && val <= '9') {
+            result = values[p.getPos()].updateValue(val);
+        }
+        else {
+            throw new ValueOutOfBoundException(val, 9);
+        }
+        return result;
+    }
+
+    public void lock() {
+        locked = true;
+        for(Cellule c : values) {
+            if(c.getValue() != EMPTY) c.lock();
+        }
+    }
+
+    public boolean isLocked() {
+        return locked;
     }
 }
