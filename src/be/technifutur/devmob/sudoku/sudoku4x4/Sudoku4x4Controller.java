760d166fc2b86c8bc6751e0fa1f290ad7e141d11
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 14:02:42 +0100

    Added the possibility to delete values for a given position

--- src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4Controller.java
+++ src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4Controller.java
@@ -43,18 +43,12 @@ public class Sudoku4x4Controller {
                 if(match.lookingAt()) {
                     entry = match.group("val");
                 }
-                Pattern splitter = Pattern.compile("\\.");
-                String[] regexValues = splitter.split(entry);
+                String[] regexValues = stringSplitter(entry);
                 int row = Integer.parseInt(regexValues[0]);
                 int col = Integer.parseInt(regexValues[1]);
                 char value = regexValues[2].charAt(0);
                 boolean result = false;
-                boolean validPosition = false;
-                try {
-                    validPosition = Position4x4.isValid(col - 1, row - 1);
-                } catch (PositionInvalidException e) {
-                    System.out.println(e.getMessage());
-                }
+                boolean validPosition = handlePositionValidation(col, row);
                 if(validPosition && addMatcher.matches()) {
                     try {
                         result = model.add(new Position4x4(col - 1, row - 1), value);
@@ -84,7 +78,45 @@ public class Sudoku4x4Controller {
                     handleNotAdded(value, row, col);
                 }
             }
+            else {
+                Matcher match = Pattern.compile("d?(?<val>\\d\\.\\d)").matcher(entry);
+                if(match.lookingAt()) {
+                    entry = match.group("val");
+                }
+                String[] regexValues = stringSplitter(entry);
+                int row = Integer.parseInt(regexValues[0]);
+                int col = Integer.parseInt(regexValues[1]);
+                boolean result = false;
+                boolean validPosition = handlePositionValidation(col, row);
+                if(validPosition) {
+                    result = model.delete(new Position4x4(col - 1, row - 1));
+                }
+                if(result) {
+                    view.update();
+                    handleDelete(row, col);
+                    view.showSudoku();
+                }
+            }
         }
+    }
+
+    private void handleDelete(int row, int col) {
+        view.show(String.format("La valeur à la ligne %d et la colonne %d a été retirée", row, col));
+    }
+
+    private boolean handlePositionValidation(int col, int row) {
+        boolean result = false;
+        try {
+            result = Position4x4.isValid(col - 1, row - 1);
+        } catch (PositionInvalidException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    private String[] stringSplitter(String entry) {
+        Pattern splitter = Pattern.compile("\\.");
+        return splitter.split(entry);
     }
 
     private void handleComplete(char value, int row, int col) {
