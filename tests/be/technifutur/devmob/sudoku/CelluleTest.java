f5707e32e06398056b4fb1376223bdcc4819cb26
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 09:14:25 +0100

    Modified the tests to throw ValueAlreadyDefinedException and CellLockedException

--- tests/be/technifutur/devmob/sudoku/CelluleTest.java
+++ tests/be/technifutur/devmob/sudoku/CelluleTest.java
@@ -25,7 +25,7 @@ class CelluleTest {
         Test N°2 : Testing if a value can be retrieved after being added
      */
     @Test
-    void testValueCanBeRetrievedAfterBeingAdded() throws ValueAlreadyDefinedException {
+    void testValueCanBeRetrievedAfterBeingAdded() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.setValue('1');
@@ -36,7 +36,7 @@ class CelluleTest {
         Test n°3 : Testing if a value can be added once with one ValueSet
      */
     @Test
-    void testValueCanBeAddedOnceWithOneValueSet() throws ValueAlreadyDefinedException {
+    void testValueCanBeAddedOnceWithOneValueSet() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         assertTrue(cell.setValue('1'));
@@ -46,7 +46,7 @@ class CelluleTest {
         Test N°4 : Testing if a value cannot be added more than once with one ValueSet
      */
     @Test
-    void testValueCannotBeAddedMoreThanOnceWithOneValueSet() throws ValueAlreadyDefinedException {
+    void testValueCannotBeAddedMoreThanOnceWithOneValueSet() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.setValue('2');
@@ -57,7 +57,7 @@ class CelluleTest {
         Test N°5 : Testing if a value can be added once with more than one ValueSet
      */
     @Test
-    void testValueCanBeAddedOnceWithMoreThanOneValueSet() throws ValueAlreadyDefinedException {
+    void testValueCanBeAddedOnceWithMoreThanOneValueSet() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.addValueSet(new ValueSet(4));
@@ -68,7 +68,7 @@ class CelluleTest {
         Test N°6 : Testing if a value cannot be added more than once with more than one ValueSet
      */
     @Test
-    void testValueCannotBeSetMoreThanOnceWithMoreThanOneValueSet() throws ValueAlreadyDefinedException {
+    void testValueCannotBeSetMoreThanOnceWithMoreThanOneValueSet() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.addValueSet(new ValueSet(4));
@@ -80,7 +80,7 @@ class CelluleTest {
         Test N°7 : Testing if two different values can be add
      */
     @Test
-    void testTwoDifferentValuesCanBeAdded() throws ValueAlreadyDefinedException {
+    void testTwoDifferentValuesCanBeAdded() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.addValueSet(new ValueSet(4));
@@ -92,7 +92,7 @@ class CelluleTest {
         Test N°8 : Testing if two distinct Cells with one similar ValueSet cannot add more than once the same value
      */
     @Test
-    void testTwoCellsWithSameValueSetCannotAddSameValue() throws ValueAlreadyDefinedException {
+    void testTwoCellsWithSameValueSetCannotAddSameValue() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell1 = new Cellule();
         Cellule cell2 = new Cellule();
         ValueSet vs = new ValueSet(4);
@@ -106,7 +106,7 @@ class CelluleTest {
         Test N°9 : Testing if two distinct Cells with one similar ValueSet can add two distinct values
      */
     @Test
-    void testTwoCellsWithSameValueSetCanAddTwoDifferentValues() throws ValueAlreadyDefinedException {
+    void testTwoCellsWithSameValueSetCanAddTwoDifferentValues() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell1 = new Cellule();
         Cellule cell2 = new Cellule();
         ValueSet vs = new ValueSet(4);
@@ -129,7 +129,7 @@ class CelluleTest {
         Test N°11 : Testing if a Cell cannot be modified after being locked
      */
     @Test
-    void testCellCannotBeModifiedAfterBeingLocked() throws ValueAlreadyDefinedException {
+    void testCellCannotBeModifiedAfterBeingLocked() throws ValueAlreadyDefinedException, CellLockedException {
         Cellule cell = new Cellule();
         cell.addValueSet(new ValueSet(4));
         cell.lock();
