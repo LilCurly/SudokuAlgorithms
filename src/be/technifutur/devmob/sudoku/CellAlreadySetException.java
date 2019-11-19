ed8cde3489534196826e4b6800192c909d3eaad7
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 12:19:00 +0100

    Fixed indexes starting at 0 : incremented indexes

--- src/be/technifutur/devmob/sudoku/CellAlreadySetException.java
+++ src/be/technifutur/devmob/sudoku/CellAlreadySetException.java
@@ -2,6 +2,6 @@ package be.technifutur.devmob.sudoku;
 
 public class CellAlreadySetException extends SudokuException {
     public CellAlreadySetException(int row, int col) {
-        super(String.format("Cell at row %d and col %d is already set. Try updating it", row, col));
+        super(String.format("Cell at row %d and col %d is already set. Try updating it", row+1, col+1));
     }
 }
--- src/be/technifutur/devmob/sudoku/PositionInvalidException.java
+++ src/be/technifutur/devmob/sudoku/PositionInvalidException.java
@@ -2,10 +2,10 @@ package be.technifutur.devmob.sudoku;
 
 public class PositionInvalidException extends SudokuException {
     public PositionInvalidException(int row, int col) {
-        super(String.format("Position is invalid : row should be between 1 and 4 (but was %d), col should be between 1 and 4 (but was %d)", row, col));
+        super(String.format("Position is invalid : row should be between 1 and 4 (but was %d), col should be between 1 and 4 (but was %d)", row+1, col+1));
     }
 
     public PositionInvalidException(int pos) {
-        super(String.format("Position is invalid : pos should be between 1 and 16 (but was %d)", pos));
+        super(String.format("Position is invalid : pos should be between 1 and 16 (but was %d)", pos+1));
     }
 }
