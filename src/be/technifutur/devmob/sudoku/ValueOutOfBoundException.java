78ddf4efb8e0ab4245c4e7a01e4d635196b5ae73
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 12:27:05 +0100

    Fixed incorrect type in argument

--- src/be/technifutur/devmob/sudoku/ValueOutOfBoundException.java
+++ src/be/technifutur/devmob/sudoku/ValueOutOfBoundException.java
@@ -4,8 +4,8 @@ public class ValueOutOfBoundException extends SudokuException {
 
     private int val;
 
-    public ValueOutOfBoundException(int val, int max) {
-        super(String.format("Value should be between 1 and %d, but you gave %d", max, val));
+    public ValueOutOfBoundException(char val, int max) {
+        super(String.format("Value should be between 1 and %d, but you gave %s", max, val));
         this.val = val;
     }
 
