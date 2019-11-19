46aa4985bdfe78ec1c8b35345d59165a431c8656
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-19 14:12:51 +0100

    Fixed message shown to something more generic

--- src/be/technifutur/devmob/sudoku/CellNotSetException.java
+++ src/be/technifutur/devmob/sudoku/CellNotSetException.java
@@ -2,6 +2,6 @@ package be.technifutur.devmob.sudoku;
 
 public class CellNotSetException extends SudokuException {
     public CellNotSetException(int row, int col) {
-        super(String.format("Cell at row %d and col %d has no value. Try setting it to a value before updating it.", row+1, col+1));
+        super(String.format("Cell at row %d and col %d has no value. Try setting it to a value.", row+1, col+1));
     }
 }
