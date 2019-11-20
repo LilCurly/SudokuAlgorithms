394c56bd1a1ba997c2398a7abb6c7e348af95cb5
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-20 16:22:24 +0100

    Removed the common code from the Sudoku4x4 model and made it extend AbstractSudoku -> implemented isValid() to check if a given value is valid in this model

--- src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4.java
+++ src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4.java
@@ -1,84 +1,15 @@
 package be.technifutur.devmob.sudoku.sudoku4x4;
 
-import be.technifutur.devmob.sudoku.*;
+import be.technifutur.devmob.sudoku.sudoku_interfaces.AbstractSudoku;
 
-public class Sudoku4x4 {
-    public static final char EMPTY = '.';
-    private Cellule[] values = new Cellule[16];
-    private boolean locked;
+public class Sudoku4x4 extends AbstractSudoku {
 
     public Sudoku4x4() {
-        locked = false;
+        super(16);
     }
 
-    public void setCellule(Position4x4 p, Cellule c) {
-        values[p.getPos()] = c;
-    }
-
-    public Cellule getCellule(Position4x4 p) {
-        return values[p.getPos()];
-    }
-
-    public boolean add(Position4x4 p, char val) throws SudokuException {
-        boolean result = false;
-        if(values[p.getPos()].getValue() != EMPTY) {
-            throw new CellAlreadySetException(p.getRow(), p.getCol());
-        }
-        if(val >= '1' && val <= '4') {
-            result = values[p.getPos()].setValue(val);
-        }
-        else {
-            throw new ValueOutOfBoundException(val, 4);
-        }
-        return result;
-    }
-
-    public char get(Position4x4 p) {
-        return values[p.getPos()].getValue();
-    }
-
-    public boolean isComplete() {
-        boolean result = true;
-        for(Cellule c : values) {
-            if(c.getValue() == EMPTY) result = false;
-        }
-        return result;
-    }
-
-    public Cellule[] getValues() {
-        return this.values;
-    }
-
-    public boolean delete(Position4x4 p) throws SudokuException {
-        boolean result = values[p.getPos()].deleteValue();
-        if(!result) {
-            throw new CellNotSetException(p.getRow(), p.getCol());
-        }
-        return result;
-    }
-
-    public boolean update(Position4x4 p, char val) throws SudokuException {
-        boolean result = false;
-        if(this.values[p.getPos()].getValue() == EMPTY) {
-            throw new CellNotSetException(p.getRow(), p.getCol());
-        }
-        if(val >= '1' && val <= '4') {
-            result = values[p.getPos()].updateValue(val);
-        }
-        else {
-            throw new ValueOutOfBoundException(val, 4);
-        }
-        return result;
-    }
-
-    public void lock() {
-        locked = true;
-        for(Cellule c : values) {
-            if(c.getValue() != EMPTY) c.lock();
-        }
-    }
-
-    public boolean isLocked() {
-        return locked;
+    @Override
+    public boolean isValid(char val) {
+        return val >= '1' && val <= '4';
     }
 }
