6ab43941b100bf5a35330203422eed858439471d
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-20 12:57:47 +0100

    Added isValid() function : checks if a position for a given row and column is valid (should throw a PositionInvalidException if it is not valid)

--- src/be/technifutur/devmob/sudoku/sudokuEtoile/PositionEtoile.java
+++ src/be/technifutur/devmob/sudoku/sudokuEtoile/PositionEtoile.java
@@ -13,6 +13,31 @@ public class PositionEtoile {
         setPos(row, col);
     }
 
+    public static boolean isValid(int row, int col) {
+        boolean result = col >= 0 && col <= 20;
+        // CASE 1
+        if(result && row >= 0 && row <= 5) {
+            result = col < 9 || col > 11;
+        }
+        // CASE 2
+        else if(result && row >= 6 && row <= 8) {
+            result = true;
+        }
+        // CASE 3
+        else if(result && row >= 9 && row <= 11) {
+            result = col > 5 && col < 15;
+        }
+        // CASE 4
+        else if(result && row >= 12 && row <= 14) {
+            result = true;
+        }
+        // CASE 5
+        else if(result && row >= 15 && row <= 20) {
+            result = col < 9 || col > 11;
+        }
+        return result;
+    }
+
     private void setPos(int row, int col) {
         // CASE 1
         if(row >= 0 && row <= 5) {
