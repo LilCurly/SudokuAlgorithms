87a6d953b8eea944f58f3a0afde933cecc2cc057
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-20 16:25:32 +0100

    Replaced the dependecy to Sudoku9x9 by a dependecy to VisualSudoku which only declare the needed methods for the view

--- src/be/technifutur/devmob/sudoku/sudoku9x9/Sudoku9x9Vue.java
+++ src/be/technifutur/devmob/sudoku/sudoku9x9/Sudoku9x9Vue.java
@@ -1,36 +1,36 @@
 package be.technifutur.devmob.sudoku.sudoku9x9;
 
 import be.technifutur.devmob.sudoku.Cellule;
+import be.technifutur.devmob.sudoku.sudoku_interfaces.VisualSudoku;
 import be.technifutur.devmob.sudoku.utils.User;
 import be.technifutur.devmob.sudoku.utils.UserConsole;
 
 
 public class Sudoku9x9Vue {
     private StringBuilder sb;
-    private Sudoku9x9 model;
+    private VisualSudoku model;
     private User user;
 
-    public Sudoku9x9Vue(Sudoku9x9 model) {
+    public Sudoku9x9Vue(VisualSudoku model) {
         this.model = model;
         this.user = new UserConsole();
         update();
     }
 
     public void update() {
-        Cellule[] vals = model.getValues();
         sb = new StringBuilder();
         sb.append(              "+---------+---------+---------+\n");
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[0].getValue(), vals[1].getValue(), vals[2].getValue(), vals[3].getValue(), vals[4].getValue(), vals[5].getValue(), vals[6].getValue(), vals[7].getValue(), vals[8].getValue()));
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[9].getValue(), vals[10].getValue(), vals[11].getValue(), vals[12].getValue(), vals[13].getValue(), vals[14].getValue(), vals[15].getValue(), vals[16].getValue(), vals[17].getValue()));
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[18].getValue(), vals[19].getValue(), vals[20].getValue(), vals[21].getValue(), vals[22].getValue(), vals[23].getValue(), vals[24].getValue(), vals[25].getValue(), vals[26].getValue()));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(0)), model.get(new Position9x9(1)), model.get(new Position9x9(2)), model.get(new Position9x9(3)), model.get(new Position9x9(4)), model.get(new Position9x9(5)), model.get(new Position9x9(6)), model.get(new Position9x9(7)), model.get(new Position9x9(8))));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(9)), model.get(new Position9x9(10)), model.get(new Position9x9(11)), model.get(new Position9x9(12)), model.get(new Position9x9(13)), model.get(new Position9x9(14)), model.get(new Position9x9(15)), model.get(new Position9x9(16)), model.get(new Position9x9(17))));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(18)), model.get(new Position9x9(19)), model.get(new Position9x9(20)), model.get(new Position9x9(21)), model.get(new Position9x9(22)), model.get(new Position9x9(23)), model.get(new Position9x9(24)), model.get(new Position9x9(25)), model.get(new Position9x9(26))));
         sb.append(              "+---------+---------+---------+\n");
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[27].getValue(), vals[28].getValue(), vals[29].getValue(), vals[30].getValue(), vals[31].getValue(), vals[32].getValue(), vals[33].getValue(), vals[34].getValue(), vals[35].getValue()));
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[36].getValue(), vals[37].getValue(), vals[38].getValue(), vals[39].getValue(), vals[40].getValue(), vals[41].getValue(), vals[42].getValue(), vals[43].getValue(), vals[44].getValue()));
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[45].getValue(), vals[46].getValue(), vals[47].getValue(), vals[48].getValue(), vals[49].getValue(), vals[50].getValue(), vals[51].getValue(), vals[52].getValue(), vals[53].getValue()));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(27)), model.get(new Position9x9(28)), model.get(new Position9x9(29)), model.get(new Position9x9(30)), model.get(new Position9x9(31)), model.get(new Position9x9(32)), model.get(new Position9x9(33)), model.get(new Position9x9(34)), model.get(new Position9x9(35))));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(36)), model.get(new Position9x9(37)), model.get(new Position9x9(38)), model.get(new Position9x9(39)), model.get(new Position9x9(40)), model.get(new Position9x9(41)), model.get(new Position9x9(42)), model.get(new Position9x9(43)), model.get(new Position9x9(44))));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(45)), model.get(new Position9x9(46)), model.get(new Position9x9(47)), model.get(new Position9x9(48)), model.get(new Position9x9(49)), model.get(new Position9x9(50)), model.get(new Position9x9(51)), model.get(new Position9x9(52)), model.get(new Position9x9(53))));
         sb.append(              "+---------+---------+---------+\n");
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[54].getValue(), vals[55].getValue(), vals[56].getValue(), vals[57].getValue(), vals[58].getValue(), vals[59].getValue(), vals[60].getValue(), vals[61].getValue(), vals[62].getValue()));
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[63].getValue(), vals[64].getValue(), vals[65].getValue(), vals[66].getValue(), vals[67].getValue(), vals[68].getValue(), vals[69].getValue(), vals[70].getValue(), vals[71].getValue()));
-        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", vals[72].getValue(), vals[73].getValue(), vals[74].getValue(), vals[75].getValue(), vals[76].getValue(), vals[77].getValue(), vals[78].getValue(), vals[79].getValue(), vals[80].getValue()));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(54)), model.get(new Position9x9(55)), model.get(new Position9x9(56)), model.get(new Position9x9(57)), model.get(new Position9x9(58)), model.get(new Position9x9(59)), model.get(new Position9x9(60)), model.get(new Position9x9(61)), model.get(new Position9x9(62))));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(63)), model.get(new Position9x9(64)), model.get(new Position9x9(65)), model.get(new Position9x9(66)), model.get(new Position9x9(67)), model.get(new Position9x9(68)), model.get(new Position9x9(69)), model.get(new Position9x9(70)), model.get(new Position9x9(71))));
+        sb.append(String.format("| %s  %s  %s | %s  %s  %s | %s  %s  %s |\n", model.get(new Position9x9(72)), model.get(new Position9x9(73)), model.get(new Position9x9(74)), model.get(new Position9x9(75)), model.get(new Position9x9(76)), model.get(new Position9x9(77)), model.get(new Position9x9(78)), model.get(new Position9x9(79)), model.get(new Position9x9(80))));
         sb.append(              "+---------+---------+---------+\n");
     }
 
