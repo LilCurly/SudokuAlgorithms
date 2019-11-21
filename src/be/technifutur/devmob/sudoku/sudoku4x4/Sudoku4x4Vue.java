c698b9d16b8405ab48ed04266fc993ebd8e758ca
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-21 13:30:43 +0100

    Made it extend AbstractSudokuView and removes redundant code

--- src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4Vue.java
+++ src/be/technifutur/devmob/sudoku/sudoku4x4/Sudoku4x4Vue.java
@@ -1,22 +1,19 @@
 package be.technifutur.devmob.sudoku.sudoku4x4;
 
+import be.technifutur.devmob.sudoku.sudoku_interfaces.AbstractSudokuView;
 import be.technifutur.devmob.sudoku.sudoku_interfaces.VisualSudoku;
-import be.technifutur.devmob.sudoku.utils.User;
-import be.technifutur.devmob.sudoku.utils.UserConsole;
 
-public class Sudoku4x4Vue {
-    private StringBuilder sb;
+public class Sudoku4x4Vue extends AbstractSudokuView {
     private VisualSudoku model;
-    private User user;
 
     public Sudoku4x4Vue(VisualSudoku model) {
         this.model = model;
-        this.user = new UserConsole();
         update();
     }
 
     public void update() {
-        sb = new StringBuilder();
+        setStringBuilder(new StringBuilder());
+        StringBuilder sb = getStringBuilder();
         sb.append(              "+------+------+\n");
         sb.append(String.format("| %s  %s | %s  %s |\n", model.get(new Position4x4(0)), model.get(new Position4x4(1)), model.get(new Position4x4(2)), model.get(new Position4x4(3))));
         sb.append(String.format("| %s  %s | %s  %s |\n", model.get(new Position4x4(4)), model.get(new Position4x4(5)), model.get(new Position4x4(6)), model.get(new Position4x4(7))));
@@ -24,25 +21,5 @@ public class Sudoku4x4Vue {
         sb.append(String.format("| %s  %s | %s  %s |\n", model.get(new Position4x4(8)), model.get(new Position4x4(9)), model.get(new Position4x4(10)), model.get(new Position4x4(11))));
         sb.append(String.format("| %s  %s | %s  %s |\n", model.get(new Position4x4(12)), model.get(new Position4x4(13)), model.get(new Position4x4(14)), model.get(new Position4x4(15))));
         sb.append(              "+------+------+\n");
-    }
-
-    public void showSudoku(){
-        System.out.println(sb.toString());
-    }
-
-    public void show(String message) {
-        user.show(message);
-    }
-
-    public void separator() {
-        System.out.println("---------------------");
-    }
-
-    public String prompt(String message) {
-        return user.prompt(message);
-    }
-
-    public void setUser(User user) {
-        this.user = user;
     }
 }
