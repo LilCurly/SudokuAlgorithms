a3288a7683b34a6014e108e98a2e018e70158ea1
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-25 15:33:23 +0100

    Added size(), isEmpty() and remainingValues() to interface

--- src/be/technifutur/devmob/sudoku/sudoku_interfaces/UpdatableSudoku.java
+++ src/be/technifutur/devmob/sudoku/sudoku_interfaces/UpdatableSudoku.java
@@ -2,9 +2,14 @@ package be.technifutur.devmob.sudoku.sudoku_interfaces;
 
 import be.technifutur.devmob.sudoku.SudokuException;
 
+import java.util.Set;
+
 public interface UpdatableSudoku {
     boolean isComplete();
     boolean add(Position pos, char c) throws SudokuException;
     boolean update(Position pos, char c) throws SudokuException;
     boolean delete(Position pos) throws SudokuException;
+    int size();
+    boolean isEmpty(Position p);
+    Set<Character> remainingValues();
 }
